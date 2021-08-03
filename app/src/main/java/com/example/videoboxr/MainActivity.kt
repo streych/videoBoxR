81559e7b42855e1db94bc7bca624a318db1d7c05
V radiulov <radiulovv.a@gmail.com>
2021-08-03 22:40:57 +1000

    add alert dialog( menu - About)

--- app/src/main/java/com/example/videoboxr/MainActivity.kt
+++ app/src/main/java/com/example/videoboxr/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.videoboxr
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.videoboxr.databinding.MainActivityBinding
 import com.example.videoboxr.ui.main.favorite.FavoriteFragment
@@ -13,8 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(binding.container.id, MainFragment.newInstance())
@@ -22,7 +33,10 @@ class MainActivity : AppCompatActivity() {
         }
         menuBottom()
 
+
     }
+
+
 
     private fun menuBottom(){
         binding.bottomMenuNav.setOnItemSelectedListener {
@@ -50,5 +64,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_up, menu)
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery(query , false)
+                searchItem.collapseActionView()
+                Toast.makeText(this@MainActivity, "Looking for $query", Toast.LENGTH_LONG).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about -> {
+                aboutDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun aboutDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("О программе")
+        builder.setMessage("Первая программа с применением MVVM и Kotlin")
+        builder.show()
+    }
 
 }
