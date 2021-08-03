1a5014ecce58cd05815a9035daf3ce35b65f95a5
V radiulov <radiulovv.a@gmail.com>
2021-08-03 22:06:49 +1000

    fragments replaice <fragmentLayout> from <ConstraintLayout>

--- app/src/main/java/com/example/videoboxr/MainActivity.kt
+++ app/src/main/java/com/example/videoboxr/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.videoboxr
 
-import android.app.SearchManager
-import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.widget.SearchView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.videoboxr.databinding.MainActivityBinding
 import com.example.videoboxr.ui.main.favorite.FavoriteFragment
@@ -55,28 +50,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_up, menu)
-        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchItem = menu?.findItem(R.id.search)
-        val searchView = searchItem?.actionView as SearchView
 
-        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchView.clearFocus()
-                searchView.setQuery(query, false)
-                searchItem.collapseActionView()
-                Toast.makeText(this@MainActivity, "Looking for $query", Toast.LENGTH_LONG).show()
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-        return true
-
-    }
 }
