882f4b4ede396594f2084460c76d950942637fbf
V radiulov <radiulovv.a@gmail.com>
2021-08-03 17:02:55 +1000

    create project + menu

---
+++ app/src/main/java/com/example/videoboxr/ui/main/FavoriteViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.videoboxr.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/videoboxr/ui/main/RetingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.videoboxr.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class RetingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
